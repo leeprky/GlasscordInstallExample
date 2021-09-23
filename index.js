886a2b0ebb8d36afbe77c10899a35e7c8312d8a5
Lee <71296232+leeprky@users.noreply.github.com>
2021-09-23 21:48:36 +0100

    0.42

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 require(`C:/Users/[[PC-NAME]]/powercord/injectors/../src/patcher.js`)
-require('C:/Users/[[PC-NAME]]/AppData/Local/DiscordCanary/app-1.0.41/resources/app/glasscord.asar')
+require('C:/Users/[[PC-NAME]]/AppData/Local/DiscordCanary/app-1.0.42/resources/app/glasscord.asar')
